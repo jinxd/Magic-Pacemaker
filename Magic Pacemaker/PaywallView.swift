98239c04bf0985f71192791112821e828edd45be
Jinxd <jinxd@users.noreply.github.com>
2024-08-18 20:47:46 +0200

    removed iaps

--- Magic Pacemaker/PaywallView.swift
+++ Magic Pacemaker/PaywallView.swift
@@ -10,12 +10,9 @@ import StoreKit
 
 
 struct PaywallView: View {
-    let isSubscribed: Bool
-    @State private var showWarning = true
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        if showWarning && !isSubscribed {
             NavigationStack {
                 List{
                     Section{
@@ -25,8 +22,7 @@ struct PaywallView: View {
                             .foregroundStyle(.red)
                             .listRowBackground(Color.clear)
                     }
-                    Text("There is NO direct benefit for you by subscribing.")
-                    Text("The only thing this changes are my priorities. If people actually subscribe, they show me that they like this App.")
+                    Text("You can't subscribe right now.")
                     Text("Because I am a single Person with limited time and too many ideas, I might **NEVER** implement features here like:")
                     Section("Automatic Frequency Adjustment:"){
                         Text("Adjusts the frequency of sounds automatically based on your speed and step length or RPM. Set a target speed and Magic Pacemaker will slowly adjust the frequency until you reach your target speed")
@@ -41,10 +37,7 @@ struct PaywallView: View {
                         Text("Widgets, Control Center and Lock Screen integration, ...")
                     }
                     Section{
-                        Text("AGAIN: Subscribing only changes the likelyhood of me implementing such features").foregroundStyle(.red).bold()
-                    }
-                    Section{
-                        Button("Okay, got it"){showWarning = false}
+                        Text("AGAIN: If I notice that this app is used I might add these features.").foregroundStyle(.red).bold()
                     }
                 }
                 .toolbar {
@@ -58,35 +51,10 @@ struct PaywallView: View {
                         .buttonBorderShape(.circle)
                     }
                 }
-            }
-        } else if isSubscribed {
-            SubscriptionStoreView(groupID: "magicians")
-                .subscriptionStoreControlStyle(.automatic)
-        } else {
-            NavigationStack{
-                Text("You are subscribed")
-                    .manageSubscriptionsSheet(isPresented: .constant(true), subscriptionGroupID: "magicians")
-                    .toolbar {
-                        ToolbarItem(placement: .cancellationAction) {
-                            Button{
-                                dismiss()
-                            } label: {
-                                Image(systemName: "xmark")
-                            }
-                            .buttonStyle(.bordered)
-                            .buttonBorderShape(.circle)
-                        }
-                    }
-
-            }
         }
     }
 }
 
 #Preview {
-    PaywallView(isSubscribed: false)
-}
-
-#Preview {
-    PaywallView(isSubscribed: true)
+    PaywallView()
 }
